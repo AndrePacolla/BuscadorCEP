48c059490d64fdf6f471f93327fc9a8009fcfd29
AndrePacolla <andreluizzpacolla@outlook.com>
2024-07-25 15:26:31 -0300

    adc api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import {useState} from 'react';
 import{FiSearch} from 'react-icons/fi';
 import'./style.css';
 
+import api from './services/api'
+
 function App() {
+  const [input, setInput] = useState('')
+
+ async function handleSearch(){
+
+    if(input === ''){
+      alert('Preencha algum CEP !')
+      return;
+    }
+
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response)
+
+    }catch{
+      alert('OPs erro ao buscar CEP..')
+    }
+
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -10,16 +33,18 @@ function App() {
         <input
         type="text"
         placeholder="Digite seu CEP . . ."
+        value = {input}
+        onChange = {(e)=> setInput(e.target.value)}
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color='#fff'/>
         </button>
       </div>
 
       <main className='main'>
         
-      <h2>CEP: 13067-366</h2>
+      <h2>CEP: 13067366</h2>
       <span>Rua: Maria Aparecida dos Reis Neves</span>
       <span>Complemento: Bloco 34A AP 13</span>
       <span>Bairro: Vila Padre Anchieta</span>
